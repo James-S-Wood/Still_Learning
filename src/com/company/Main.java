48126e7ac1e53271e9fb087fe9c82f2ff3ff3ce9
jameswood <james_s_wood@outlook.com>
2020-08-18 11:32:01 -0400

    Added a print line.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Still Learning");
         System.out.println("and I am still loving it");
+        System.out.println("Let's make some changes and commit");
     }
 }
