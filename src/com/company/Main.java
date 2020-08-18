31fb41d0c4f330dbc7aa3f4519275c1fd0ea2632
jameswood <james_s_wood@outlook.com>
2020-08-18 11:29:34 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Still Learning");
+        System.out.println("and I am still loving it");
+    }
+}
